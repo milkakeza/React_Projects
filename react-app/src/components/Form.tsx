b424b9045a68ad670746e73636a2938de79d51f2
milkakeza <m.isingizwe1@alustudent.com>
2025-06-03 12:17:10 +0200

    add: Mnaging Forms with React Hook Form

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,25 +1,17 @@
-import { useState, type FormEvent } from "react";
+import { useForm, type FieldValues } from "react-hook-form";
 
 const Form = () => {
-  const [person, setPerson] = useState({
-    name: "",
-    age: 0,
-  });
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(person);
-  };
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: FieldValues) => console.log(data);
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          onChange={(event) =>
-            setPerson({ ...person, name: event.target.value })
-          }
-          value={person.name}
+          {...register("name")}
           id="name"
           type="text"
           className="form-control"
@@ -30,18 +22,54 @@ const Form = () => {
           Age
         </label>
         <input
-          onChange={(event) =>
-            setPerson({ ...person, age: parseInt(event.target.value) })
-          }
-          value={person.age}
+          {...register("age")}
           id="age"
           type="number"
           className="form-control"
         />
       </div>
-      <button className="btn btn-primary" type="submit">Submit</button>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
     </form>
   );
 };
 
 export default Form;
+
+// import { useRef, type FormEvent } from "react";
+
+// const Form = () => {
+//   const nameRef = useRef<HTMLInputElement>(null);
+//   const ageRef = useRef<HTMLInputElement>(null);
+//   const person = { name: '', age: 0};
+
+//   const handleSubmit = (event: FormEvent) => {
+//     event.preventDefault();
+//     if (nameRef.current !== null) person.name = nameRef.current.value;
+//     if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+//     console.log(person);
+
+//   };
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div className="mb-3">
+//         <label htmlFor="name" className="form-label">
+//           Name
+//         </label>
+//         <input ref={nameRef} id="name" type="text" className="form-control" />
+//       </div>
+//       <div className="mb-3">
+//         <label htmlFor="age" className="form-label">
+//           Age
+//         </label>
+//         <input ref={ageRef} id="age" type="number" className="form-control" />
+//       </div>
+//       <button className="btn btn-primary" type="submit">
+//         Submit
+//       </button>
+//     </form>
+//   );
+// };
+
+// export default Form;
